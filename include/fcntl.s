76c1d692ac46a2ffc7b141b6ee3aa9ff9d2d66b7
jaacoppi <jaacoppi@kapsi.fi>
2015-08-02 17:04:33 +0300

    proper handling of file control codes (O_RDONLY etc) for both freebsd and linux

--- include/fcntl.s
+++ include/fcntl.s
@@ -7,10 +7,18 @@
 
 .ifndef FCNTL	// only include once
 .equiv FNCTL, 1
-
+.ifdef FREEBSD
+.equiv O_RDONLY, 	0x0000
+.equiv O_WRONLY, 	0x0001
+.equiv O_RDWR, 		0x0002
+.equiv O_CREAT,		0x0200
+.equiv O_APPEND,	0x0008
+.endif
+.ifdef LINUX
 .equiv O_RDONLY, 	1
 .equiv O_WRONLY, 	2
 .equiv O_RDWR, 		3
 .equiv O_CREAT,		100
 .equiv O_APPEND,	1000
+.endif
 .endif
