c3115227eaff50cce4a9e6892fd4bed324177bff
Juho Hiltunen <jaacoppi@kapsi.fi>
2017-02-04 21:06:55 +0200

    use strings instead of numbers for target osabi, file type and machine type
    Also, code cleanup with macros m_displayfield and m_displayfieldh

--- userland/readelf.s
+++ userland/readelf.s
@@ -63,10 +63,9 @@ register conventions:
 	x21 = temp for printing
 	x22 = temp for printing
 =============================================================================*/
-// TODO: for each field, print the corresponding string. like we do with endianness. Study enum for assembly.
-// TODO: make a macro to simplify code
 // magic
 	m_fputs magicstr
+
 	ldrb w21, [x20], 1
 	m_printregh x21
 	m_fputs space
@@ -79,6 +78,7 @@ register conventions:
 	ldrb w21, [x20], 1
 	m_printregh x21
 	m_fputs newline
+
 // 32/64 bit - simply multiply 1 or 2 by 32
 	m_fputs classstr
 	ldrb w21, [x20], 1
@@ -99,86 +99,118 @@ bigendian:
 
 next:
 	m_fputs newline
-// version
-	ldrb w21, [x20], 1
-	m_fputs versionstr
+
+.macro m_displayfield bytes fieldstr
+	ldrb w21, [x20], \bytes
+	m_fputs \fieldstr
 	m_printregi x21
 	m_fputs newline
-// target operating system
-	ldrb w21, [x20], 1
+.endm
+
+.macro m_displayfieldh bytes fieldstr
+	ldrb w21, [x20], \bytes
+	m_fputs \fieldstr
+	m_printregh x21
+	m_fputs newline
+.endm
+
+	m_displayfield 1 versionstr
+// Target OS ABI
 	m_fputs abistr
-	m_printregi x21
+	ldrb w21, [x20], 1
+// switch statement
+	adr x0, abistr_array
+.macro m_switch_retabi cmpwhat
+	cmp w21, \cmpwhat
+		beq retabi
+		add x0, x0, #15
+.endm
+	m_switch_retabi 0x0
+	m_switch_retabi 0x1
+	m_switch_retabi 0x2
+	m_switch_retabi 0x3
+	m_switch_retabi 0x6
+	m_switch_retabi 0x7
+	m_switch_retabi 0x8
+	m_switch_retabi 0x9
+	m_switch_retabi 0x0A
+	m_switch_retabi 0x0B
+	m_switch_retabi 0x0C
+	m_switch_retabi 0x0D
+	m_switch_retabi 0x0E
+	m_switch_retabi 0x0F
+	m_switch_retabi 0x10
+	m_switch_retabi 0x11
+	m_switch_retabi 0x53
+
+	retabi:
+	bl fputs
 	m_fputs newline
 
-// skip e_ident[EI_ABIVERSION] and e_ident[EI_PAD]
-	ldrb w21, [x20], 8
-// e_type (TODO: check that this can read 2 bytes instead of 1)
-	ldrb w21, [x20], 2
+	ldr x21, [x20], 8 // skip e_ident[EI_ABIVERSION] and e_ident[EI_PAD]
+
+// type:
 	m_fputs typestr
-	m_printregi x21
-	m_fputs newline
-// e_machine (TODO: check that this can read 2 bytes instead of 1)
 	ldrb w21, [x20], 2
-	m_fputs machinestr
-	m_printregh x21
+	// typestr_array has strings of 12 chars. Get offset from the field type
+	mov x2, #12
+	mul x1, x2, x21
+	sub x1, x1, x2 // type starts from 1. Offset starts at 0. Recalculte. offset of typestr_array now in x1
+	adr x21, typestr_array //address of typestr_array now at x21
+	add x0, x21, x1 // add address+offset to x0 for fputs
+	bl fputs
 	m_fputs newline
 
-// e_version (TODO: check that this can read 4 bytes instead of 1)
-	ldrb w21, [x20], 4
-	m_fputs eversionstr
-	m_printregh x21
+	m_fputs machinestr
+	ldrb w21, [x20], 2
+// switch statement
+	adr x0, machinestr_array
+.macro m_switch_retmachine cmpwhat
+	cmp w21, \cmpwhat
+		beq retmachine
+		add x0, x0, #8
+.endm
+	m_switch_retmachine 0x0
+	m_switch_retmachine 0x2
+	m_switch_retmachine 0x3
+	m_switch_retmachine 0x8
+	m_switch_retmachine 0x14
+	m_switch_retmachine 0x28
+	m_switch_retmachine 0x2A
+	m_switch_retmachine 0x32
+	m_switch_retmachine 0x3E
+	m_switch_retmachine 0xB7
+	m_switch_retmachine 0xF3
+	retmachine:
+	bl fputs
 	m_fputs newline
-// e_entry (TODO: check that this can read 4 bytes instead of 1)
+
+
+	m_displayfield 4 eversionstr
+
+// e_entry, e_phoff and shoff are using ldr (8 bytes) instead of lrdb, don't macro
 	ldr x21, [x20], 8
 	m_fputs entrystr
 	m_printregh x21
 	m_fputs newline
-// e_phoff
+
 	ldr x21, [x20], 8
 	m_fputs e_phoffstr
 	m_printregi x21
 	m_fputs newline
-// e_shoff
+
 	ldr x21, [x20], 8
 	m_fputs e_shoffstr
 	m_printregi x21
 	m_fputs newline
-// e_flags
-	ldrb w21, [x20], 4
-	m_fputs e_flagsstr
-	m_printregh x21
-	m_fputs newline
-// e_ehsize
-	ldrb w21, [x20], 2
-	m_fputs e_ehsizestr
-	m_printregi x21
-	m_fputs newline
-// e_phentsize
-	ldrb w21, [x20], 2
-	m_fputs e_phentsizestr
-	m_printregi x21
-	m_fputs newline
-// e_phnum
-	ldrb w21, [x20], 2
-	m_fputs e_phnumstr
-	m_printregi x21
-	m_fputs newline
-// e_shentsize
-	ldrb w21, [x20], 2
-	m_fputs e_shentsizestr
-	m_printregi x21
-	m_fputs newline
-// e_shnumize
-	ldrb w21, [x20], 2
-	m_fputs e_ehsizestr
-	m_printregi x21
-	m_fputs newline
-// e_shstrmdx
-	ldrb w21, [x20], 2
-	m_fputs e_shstrmdxstr
-	m_printregi x21
-	m_fputs newline
 
+	m_displayfieldh 4 e_flagsstr
+	m_displayfield 2 e_ehsizestr
+	m_displayfield 2 e_phentsizestr
+	m_displayfield 2 e_phnumstr
+	m_displayfield 2 e_shentsizestr
+	m_displayfield 2 e_shnumstr
+	m_displayfield 2 e_shstrmdxstr
 
 close:
 	mov x0, x19
@@ -198,25 +230,80 @@ error:
 
 .data
 filepath: .asciz "readelf"
-magicstr: .asciz "ELF Magic:\t\t"
-classstr: .asciz "Header format:\t\t"
-littlestr: .asciz "Header endianness:\tLittle endian"
-bigstr: .asciz "Header endianness:\tBig endian"
-versionstr: .asciz "Version:\t\t"
-abistr: .asciz "Target OS/ABI:\t\t"
-typestr: .asciz "Type:\t\t\t"
-machinestr: .asciz "Machine:\t\t"
-eversionstr: .asciz "E_Version:\t\t"
-entrystr: .asciz "Entry point address:\t"
+
+magicstr: .asciz "ELF Magic:\t\t\t"
+classstr: .asciz "Header format (Class):\t\t"
+littlestr: .asciz "Header endianness:\t\tLittle endian"
+bigstr: .asciz "Header endianness:\t\tBig endian"
+versionstr: .asciz "Version:\t\t\t"
+abistr: .asciz "Target OS/ABI:\t\t\t"
+abistr_array: // an array of 15-byte strings:
+	.asciz "System V"
+	.space 6
+	.asciz "HP-UX"
+	.space 9
+	.asciz "NetBSD"
+	.space 8
+	.asciz "Linux"
+	.space 9
+	.asciz "Solaris"
+	.space 7
+	.asciz "AIX"
+	.space 11
+	.asciz "IRIX"
+	.space 10
+	.asciz "FreeBSD"
+	.space 7
+	.asciz "Tru64"
+	.space 9
+	.asciz "Novell Modesto"
+	.asciz "OpenVMS"
+	.space 7
+	.asciz "OpenBSD"
+	.space 7
+	.asciz "NonStop Kernel"
+	.asciz "AROS"
+	.space 10
+	.asciz "Fenix OS"
+	.space 6
+	.asciz "CloudABI"
+	.space 6
+	.asciz "Sortix"
+	.space 8
+	.asciz "\0"
+typestr: .asciz "Type:\t\t\t\t"
+typestr_array: // an array of 12-byte strings:
+	.asciz "Relocatable"
+	.asciz "Executable\0"
+	.asciz "Shared\0\0\0\0\0"
+	.asciz "Core\0\0\0\0\0\0\0"
+machinestr: .asciz "Machine:\t\t\t"
+machinestr_array: 
+	.asciz "Unknown"
+	.asciz "SPARC\0\0"
+	.asciz "x86\0\0\0\0"
+	.asciz "MIPS\0\0\0"
+	.asciz "PowerPC"
+	.asciz "ARM\0\0\0\0"
+	.asciz "SuperH\0"
+	.asciz "IA-64\0\0"
+	.asciz "x86-64\0"
+	.asciz "AArch64"
+	.asciz "RISC-V\0"
+	.space 7
+	.asciz "\0"
+
+eversionstr: .asciz "E_Version:\t\t\t"
+entrystr: .asciz "Entry point address:\t\t"
 e_phoffstr: .asciz "Start of program headers (bytes into file):\t"
 e_shoffstr: .asciz "Start of section headers (bytes into file):\t"
-e_flagsstr: .asciz "Flags:\t\t\t"
-e_ehsizestr: .asciz "Size of this header:\t"
+e_flagsstr: .asciz "Flags:\t\t\t\t"
+e_ehsizestr: .asciz "Size of this header:\t\t"
 e_phentsizestr: .asciz "Size of program headers:\t"
 e_phnumstr: .asciz "Number of program headers:\t"
 e_shentsizestr: .asciz "Size of section headers:\t"
 e_shnumstr: .asciz "Number of section headers:\t"
-e_shstrmdxstr: .asciz "Section header string table index:\t"
+e_shstrmdxstr: .asciz "Section header string table index:\t\t"
 
 errorstr: .asciz "An error has occurred reading elf headers for file: "
 
