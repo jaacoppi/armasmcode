a0016a2054f86ba5bb487a25044dce85a873c5dc
Juho Hiltunen <jaacoppi@kapsi.fi>
2017-02-04 09:08:52 +0200

    Readelf succesfully reads the file header
    TODO:
    simplify code with macros
    use text strings instead of hexes and ints
    make output prettier
    
    Use for program and section headers as well

--- userland/readelf.s
+++ userland/readelf.s
@@ -4,6 +4,9 @@
 // Debugging ELF header parsing
 
 // NOTE: this only supports 64bit format. In 32bit, the offsets after 0x18 are different
+// Acknowledgements:
+// http://www.ouah.org/RevEng/x430.htm
+// https://en.wikipedia.org/wiki/Executable_and_Linkable_Format
 
 .include "macros.s"
 .include "globals.s"
@@ -35,6 +38,7 @@ open:
 reservemem:
 	ldr x20, =memarea
 // read the elf file from disk to memory
+// TODO: only read the required parts
 read:
 	mov x0, x20
 	mov x1, #64
@@ -59,12 +63,12 @@ register conventions:
 	x21 = temp for printing
 	x22 = temp for printing
 =============================================================================*/
+// TODO: for each field, print the corresponding string. like we do with endianness. Study enum for assembly.
+// TODO: make a macro to simplify code
 // magic
 	m_fputs magicstr
 	ldrb w21, [x20], 1
 	m_printregh x21
-	mov x0, #1
-	m_printregh x0
 	m_fputs space
 	ldrb w21, [x20], 1
 	m_printregh x21
@@ -100,7 +104,81 @@ next:
 	m_fputs versionstr
 	m_printregi x21
 	m_fputs newline
-// TODO: rest
+// target operating system
+	ldrb w21, [x20], 1
+	m_fputs abistr
+	m_printregi x21
+	m_fputs newline
+
+// skip e_ident[EI_ABIVERSION] and e_ident[EI_PAD]
+	ldrb w21, [x20], 8
+// e_type (TODO: check that this can read 2 bytes instead of 1)
+	ldrb w21, [x20], 2
+	m_fputs typestr
+	m_printregi x21
+	m_fputs newline
+// e_machine (TODO: check that this can read 2 bytes instead of 1)
+	ldrb w21, [x20], 2
+	m_fputs machinestr
+	m_printregh x21
+	m_fputs newline
+
+// e_version (TODO: check that this can read 4 bytes instead of 1)
+	ldrb w21, [x20], 4
+	m_fputs eversionstr
+	m_printregh x21
+	m_fputs newline
+// e_entry (TODO: check that this can read 4 bytes instead of 1)
+	ldr x21, [x20], 8
+	m_fputs entrystr
+	m_printregh x21
+	m_fputs newline
+// e_phoff
+	ldr x21, [x20], 8
+	m_fputs e_phoffstr
+	m_printregi x21
+	m_fputs newline
+// e_shoff
+	ldr x21, [x20], 8
+	m_fputs e_shoffstr
+	m_printregi x21
+	m_fputs newline
+// e_flags
+	ldrb w21, [x20], 4
+	m_fputs e_flagsstr
+	m_printregh x21
+	m_fputs newline
+// e_ehsize
+	ldrb w21, [x20], 2
+	m_fputs e_ehsizestr
+	m_printregi x21
+	m_fputs newline
+// e_phentsize
+	ldrb w21, [x20], 2
+	m_fputs e_phentsizestr
+	m_printregi x21
+	m_fputs newline
+// e_phnum
+	ldrb w21, [x20], 2
+	m_fputs e_phnumstr
+	m_printregi x21
+	m_fputs newline
+// e_shentsize
+	ldrb w21, [x20], 2
+	m_fputs e_shentsizestr
+	m_printregi x21
+	m_fputs newline
+// e_shnumize
+	ldrb w21, [x20], 2
+	m_fputs e_ehsizestr
+	m_printregi x21
+	m_fputs newline
+// e_shstrmdx
+	ldrb w21, [x20], 2
+	m_fputs e_shstrmdxstr
+	m_printregi x21
+	m_fputs newline
+
 
 close:
 	mov x0, x19
@@ -125,6 +203,21 @@ classstr: .asciz "Header format:\t\t"
 littlestr: .asciz "Header endianness:\tLittle endian"
 bigstr: .asciz "Header endianness:\tBig endian"
 versionstr: .asciz "Version:\t\t"
+abistr: .asciz "Target OS/ABI:\t\t"
+typestr: .asciz "Type:\t\t\t"
+machinestr: .asciz "Machine:\t\t"
+eversionstr: .asciz "E_Version:\t\t"
+entrystr: .asciz "Entry point address:\t"
+e_phoffstr: .asciz "Start of program headers (bytes into file):\t"
+e_shoffstr: .asciz "Start of section headers (bytes into file):\t"
+e_flagsstr: .asciz "Flags:\t\t\t"
+e_ehsizestr: .asciz "Size of this header:\t"
+e_phentsizestr: .asciz "Size of program headers:\t"
+e_phnumstr: .asciz "Number of program headers:\t"
+e_shentsizestr: .asciz "Size of section headers:\t"
+e_shnumstr: .asciz "Number of section headers:\t"
+e_shstrmdxstr: .asciz "Section header string table index:\t"
+
 errorstr: .asciz "An error has occurred reading elf headers for file: "
 
 // the elf header from https://en.wikipedia.org/wiki/Executable_and_Linkable_Format#File_header
@@ -157,3 +250,4 @@ e_phnum: .space 0x2 	// amount of program header tables
 e_shentsize: .space 0x2 // size of section header table
 e_shnum: .space 0x2 	// amount of section header tables
 e_shstrmdx: .space 0x2	// index of section header table containing section names
+
