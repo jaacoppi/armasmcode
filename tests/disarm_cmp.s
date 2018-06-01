51ff8278a311d991909ee3fbf56da40290b9f9ed
Juho Hiltunen <jaacoppi@kapsi.fi>
2018-06-01 19:10:02 +0300

    Add opcode and test for 5.6.42 CMN (immediate). This is actually ADDS, but alias is cmn.
    Note that CMN is used by the assembler instead of CMP when comparing negative numbers.

--- tests/disarm_cmp.s
+++ tests/disarm_cmp.s
@@ -1,10 +1,11 @@
 // aarch64 (ARM 64bit) assembly code for GNU as assembler
 //
 // file: disarm_cmp.s
-// test case for reading the opcde 0x58 (LDR)
+// test cases for dissassembling opcodes related to mnemonic CMP
 
 .global _start
 .balign 4
 .text
 _start:
 cmp x28, 0xFF
+cmp x10, 0xFFFFFFFFFFFFFFFD 	// -3, changes cmp disassembly to cmn
