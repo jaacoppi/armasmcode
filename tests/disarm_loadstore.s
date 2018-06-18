136c61436e1dc3cbddaa756ee6844d5b9d016995
Juho Hiltunen <jaacoppi@kapsi.fi>
2018-06-18 19:12:25 +0300

    5.6.180 STRB, post index

--- tests/disarm_loadstore.s
+++ tests/disarm_loadstore.s
@@ -15,6 +15,7 @@ ldr w10, [x20]		// 5.6.83, immediate offset, 32bit
 ldrb w17, [x29]
 ldrb w1, [x17], -0x20
 strb w10, [x1]		// 5.6.180, STRB (immediate). This is an offset variant, currently doesn't handle offsets
+strb w10, [x1], -0x1		// 5.6.180, STRB (immediate), post index
 adr x29, 4	// byte align 4
 address:
 
